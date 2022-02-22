49254ca11ef7be1723645d6422b613647cd24ca8
rshetty22166145 <62463648+rshetty22166145@users.noreply.github.com>
2022-02-22 10:06:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import sqlite3
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -37,11 +37,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://")
-db = SQL(uri)
-
+db = SQL("sqlite:///finance.db")
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
